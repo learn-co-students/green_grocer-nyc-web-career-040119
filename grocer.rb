2f410d2a2c9f921beeea1ad38628d019a1832b6c
Danielle Jasper <dnj547@live.com>
2019-03-17 14:57:49 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -1,15 +1,58 @@
+require 'pry'
 def consolidate_cart(cart)
-  # code here
+  new_hash = {}
+  cart.each do |item|
+    item.each do |name, attributes|
+      if !new_hash[name]
+        new_hash[name] = attributes
+        new_hash[name][:count] = 1
+      elsif new_hash[name]
+        new_hash[name][:count] += 1
+      end
+    end
+  end
+  new_hash
 end
 
 def apply_coupons(cart, coupons)
-  # code here
+  coupons.each do |coupon|
+    name = coupon[:item]
+    if cart[name] && cart[name][:count] >= coupon[:num]
+      if cart["#{name} W/COUPON"]
+        cart["#{name} W/COUPON"][:count] += 1
+      else
+        cart["#{name} W/COUPON"] = {
+          :count => 1,
+          :price => coupon[:cost]
+        }
+        cart["#{name} W/COUPON"][:clearance] = cart[name][:clearance]
+      end
+      cart[name][:count] -= coupon[:num]
+    end
+  end
+  cart
 end
 
 def apply_clearance(cart)
-  # code here
+  cart.each do |item|
+    if item[1][:clearance]
+      new_price = item[1][:price] * 0.8
+      item[1][:price] = new_price.round(1)
+    end
+  end
 end
 
 def checkout(cart, coupons)
-  # code here
+  cart1 = consolidate_cart(cart)
+  cart2 = apply_coupons(cart1, coupons)
+  cart3 = apply_clearance(cart2)
+  price_array = []
+  cart3.each do |name, attributes|
+    price_array << attributes[:price] * attributes[:count]
+  end
+  if price_array.inject(:+) > 100
+    price_array.inject(:+) * 0.9
+  else
+    price_array.inject(:+)
+  end
 end
